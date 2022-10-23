9d09f4854d4554fbedbbfa8088c49d6345af8299
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2022-10-23 23:50:55 +0530

    RPST GAME
    Rock paper scissors (also known by other orderings of the three items, with "rock" sometimes being called "stone," or as Rochambeau, roshambo, or ro-sham-bo)[1][2][3] is a hand game originating from China, usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are "rock" (a closed fist), "paper" (a flat hand), and "scissors" (a fist with the index finger and middle finger extended, forming a V). "Scissors" is identical to the two-fingered V sign (also indicating "victory" or "peace") except that it is pointed horizontally instead of being held upright in the air.
    
    A simultaneous, zero-sum game, it has three possible outcomes: a draw, a win or a loss. A player who decides to play rock will beat another player who has chosen scissors ("rock crushes scissors" or "breaks scissors" or sometimes "blunts scissors"[4]), but will lose to one who has played paper ("paper covers rock"); a play of paper will lose to a play of scissors ("scissors cuts paper"). If both players choose the same shape, the game is tied and is usually immediately replayed to break the tie. The game spread from China while developing different variants in signs over time.
    
    Rock paper scissors is often used as a fair choosing method between two people, similar to coin flipping, drawing straws, or throwing dice in order to settle a dispute or make an unbiased group decision. Unlike truly random selection methods, however, rock paper scissors can be played with a degree of skill by recognizing and exploiting non-random behavior in opponents

---
+++ rock-paper-scissor/RPST.py
@@ -0,0 +1,79 @@
+import random
+import os
+from termcolor import colored
+from pyfiglet import Figlet
+os.system("cls")
+f = Figlet(font="slant")
+print(colored(f.renderText("RPST GAME"),"green")) 
+score=0
+compscore=0
+list1=["rock","paper","scissor","thread"]
+computer=random.choice(list1)
+while True:
+    computer=random.choice(list1)
+    user = str(input("enter your choice(rock,paper,scissor,thread):"))
+    print("you choice :",user)
+    print("computer choice:",computer)
+    if user == computer:
+        print("tie")
+    elif user == 'rock':
+        if computer == "scissor":
+            print("you win")
+            score += 1   
+            compscore -= 0.25 
+        elif computer == "thread":
+            print("you lose")
+            score -=0.25
+            compscore += 1
+        else :
+            print("you lose")
+            score -=0.25
+            compscore += 1   
+    elif user == "scissor":
+        if computer == "paper":
+            print("you win")
+            score += 1
+            compscore -= 0.25
+        elif computer == "thread":
+            print("you win")
+            score += 1
+            compscore -= 0.25
+        else:
+            print("you lose")
+            score -= 0.25 
+            compscore += 1
+    elif user == "paper":
+        if computer == "rock":
+            print("you win")
+            score += 1
+            compscore -= 0.25
+        elif computer == "thread":
+            print("you win")
+            score += 1
+            compscore -= 0.25 
+        else:
+            print("you lose")
+            score -= 0.25
+            compscore += 1
+    elif user == "thread":
+        if computer == "rock":
+            print("you win")
+            score += 1
+            compscore -= 0.25 
+        elif computer == "paper":
+            print("you win")
+            score += 1
+            compscore -= 0.25
+        else:
+            print("you lose")    
+            score -= 0.25
+            compscore += 1                 
+    elif user == "exit":
+        print("have a good day ma'am/sir")
+        print(score)
+        print(compscore) 
+        break
+    else :
+        print("wrong input please try again")
+    print("your score =",score)
+    print("computer score =",compscore)                                 
